19e44fcd52a00710f770e0984f2ddf309ab2879b
Gaurav Goswami <142787056+itzgauravgoswami@users.noreply.github.com>
2024-07-25 18:03:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    const headbar = document.querySelector('.headbar');
-    const menubtn = document.querySelector('.menubtn');
-    const hamburger = document.querySelector('.hamburger');
-
-    headbar.classList.toggle('active');
-    menubtn.classList.toggle('active');
-    hamburger.classList.toggle('active');
-});
